b66d9a83c8333c4f77db6954d4e0167388ab365a
Eric Stratton <strattonericj@gmail.com>
2020-10-28 16:05:39 -0700

    add code for test of regular expression to recognize vowels
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- js/tests.js
+++ js/tests.js
@@ -1,7 +1,8 @@
 Describe: `pigLatin()`
-Test: "It will convert letters to lowercase."
-Expect(pigLatin("hElLo").toEqual("hello"));
-Test: "It will recognize ["a", "e", "i", "o", "u"] as vowels."
+Test: "It will ignore non-alphabetical characters."
+Expect:
+Test: "It will recognize ["a", "e", "i", "o", "u"] as vowels regardles of case."
+Code: /[aeiou]/gi.test(string);
 Expect(pigLatin("a").toEqual(true));
 Test: "It will add 'way' to the end of words that begin with a vowel."
 Expect(pigLatin("a").toEqual("away"));
@@ -9,3 +10,6 @@ Test: "It will move all consecutive consonants from beginning of sentence to end
 Expect(pigLatin("cat").toEqual("atcay"));
 Test: "It will move 'u', along with 'q' if 'qu' is included in first consonants, and add 'ay'."
 Expect(pigLatin("queen").toEqual("eenquay"));
+//add test for multiple words and white space
+Test: "It will ignore whitespace in a multi-word sentence"
+
