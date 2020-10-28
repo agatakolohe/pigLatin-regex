c3bb2a78617112ded9566f9564d11d1853a2fdda
Eric Stratton <strattonericj@gmail.com>
2020-10-28 14:05:37 -0700

    add initial js, css, and html, includes tests.js to test pig latin specs
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

---
+++ js/tests.js
@@ -0,0 +1,28 @@
+// Practice Ordering Manual Tests from least-to-most complexity: //
+
+
+Describe: vowelCounter();
+
+Test: "It recognizes vowels in a multiple word sentence regardless of capitalization."
+Expect(vowelCounter("CATS CATERED THE EVENT")).toEqual(7);
+
+Test: "It recognizes a single vowel in a word with multiple characters."
+Expect(vowelCounter("cat")).toEqual(1);
+
+1) Test: "It recognizes a single vowel."
+Expect(vowelCounter("a")).toEqual(1);
+
+Test: "It recognizes multiple vowels in a single word."
+Expect(vowelCounter("cater")).toEqual(2);
+
+Test: "It recognizes a single vowel regardless of case."
+Expect(vowelCounter("A")).toEqual(1);
+
+Test: "It recognizes all vowels in a multiple-word sentence regardless of inconsistent capitalization."
+Expect(vowelCounter("CaTS CATEReD ThE EveNT")).toEqual(7);
+
+Test: "It ignores non-alphabetical characters since they can't be vowels."
+Expect(vowelCounter("*&$92%")).toEqual(0);
+
+Test: "It recognizes vowels in a multiple-word sentence."
+Expect(vowelCounter("cats catered the event")).toEqual(7);
